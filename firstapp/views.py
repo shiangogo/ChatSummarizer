8f199c663e22e6178800555b4a5749d31a9dae0b
Andrew Fei <andrew77498864@gmail.com>
2023-05-29 18:03:10 +0800

    Add log to Google Sheets

--- firstapp/views.py
+++ firstapp/views.py
@@ -7,8 +7,16 @@ from linebot import LineBotApi, WebhookParser
 from linebot.exceptions import InvalidSignatureError, LineBotApiError
 from linebot.models import MessageEvent, TextSendMessage
 
+from datetime import datetime
+
+import gspread
+
 line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
 parser = WebhookParser(settings.LINE_CHANNEL_SECRET)
+
+service_account = gspread.service_account(filename=settings.GDOCS_OAUTH_JSON)
+db = service_account.open(settings.GDOCS_SPREADSHEET_NAME)
+table = db.worksheet(settings.GDOCS_WORKSHEET_NAME)
 
 
 @csrf_exempt
@@ -27,11 +35,20 @@ def callback(request):
         for event in events:
             print(event)
             if isinstance(event, MessageEvent):
-                user_name = line_bot_api.get_profile(event.source.user_id)
-                resp_message = user_name.display_name + "說：" + event.message.text
+                message_dict = {}
+                message_dict["user_id"] = event.source.user_id[:8]
+                message_dict["message"] = event.message.text
+                message_dict["sent_at"] = event.timestamp
+                try:
+                    message_dict["group_id"] = event.source.group_id
+                except AttributeError:
+                    message_dict["group_id"] = ""
+                    pass
+
+                resp_message = "用戶ID：" + message_dict["user_id"] + " 說：「" + message_dict["message"] + "」"
+                table.append_row([message_dict["group_id"], message_dict["user_id"], message_dict["message"], message_dict["sent_at"]])
 
                 line_bot_api.reply_message(event.reply_token,TextSendMessage(text=resp_message))
-
                 
         return HttpResponse()
     else:
