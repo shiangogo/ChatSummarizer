a976c672f56c848ae8e6b0747e22d26e0cf14355
Andrew Fei <andrew77498864@gmail.com>
2023-05-30 16:30:28 +0800

    Add support for recording sticker and image

--- firstapp/views.py
+++ firstapp/views.py
@@ -33,11 +33,12 @@ def callback(request):
             return HttpResponseBadRequest()
 
         for event in events:
+            print(event)
             is_in_group = event.source.type == "group"
             message_dict = {}
             col_labels = table.row_values(1)
 
-            if event.type == "message" and event.message.type == "text":
+            if event.type == "message":
                 if is_in_group:
                     message_dict["group_id"] = event.source.group_id
                     message_dict["group_name"] = line_bot_api.get_group_summary(event.source.group_id).group_name
@@ -49,14 +50,22 @@ def callback(request):
 
                 message_dict["id"] = event.message.id
                 message_dict["user_id"] = event.source.user_id
-                message_dict["message"] = event.message.text
                 message_dict["sent_at"] = event.timestamp
                 message_dict["unsent_at"] = ""
 
+                if event.message.type == "text":
+                    message_dict["message"] = event.message.text
+                elif event.message.type == "sticker":
+                    keywords = ", ".join(event.message.keywords)
+                    message_dict["message"] = f"（傳送了一個{keywords}的貼圖）"
+                elif event.message.type == "image":
+                    message_dict["message"] = "（傳送了一張圖片）"
+                else:
+                    message_dict["message"] = ""
 
                 table.append_row([message_dict.get(key) for key in ["id", "group_id", "group_name", "user_id", "user_name", "message", "sent_at", "unsent_at"]])
 
-                # resp_message = "用戶名：" + message_dict["user_name"] + " 說：「" + message_dict["message"] + "」"
+                # resp_message = message_dict["message"]
                 # line_bot_api.reply_message(event.reply_token,TextSendMessage(text=resp_message))
             if event.type == "unsend":
                 try:
